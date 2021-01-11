f1f7cd41ae591646e79787df571f62db6ce38c1f
ancasuliman <anca.suliman15@yahoo.com>
2021-01-11 22:19:40 +0200

    sidebar cu calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Sidebar from "./components/sidebar/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Products from "./components/pages/Products";
@@ -9,7 +8,6 @@ import Reports from "./components/pages/Reports";
 function App() {
     return (
         <Router>
-            <Sidebar />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/products" component={Products} />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SatMap from "../map/SatMap";
+import Sidebar from "../sidebar/Sidebar";
 
 function Home() {
     return (
         <div className="home">
+            <Sidebar />
             <SatMap></SatMap> 
         </div>
     );
