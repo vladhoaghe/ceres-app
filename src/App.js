e9f0b7aefbc87c4b3d202b4cff10e3b6c5b82c74
ancasuliman <anca.suliman15@yahoo.com>
2021-01-11 20:49:24 +0200

    sidebar blana peste harta

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Reports from "./components/pages/Reports";
 function App() {
     return (
         <Router>
-            {/* <Sidebar/> */}
+            <Sidebar />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/products" component={Products} />
